25181ac7a65bbf1c67416fe9cffdcb0ca0f5c726
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2024-09-09 15:37:38 +0300

    Main and module_2_2 files added. Conditional operator task done.

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first=int(input('Введите первое число и нажмите Enter '))
+second=int(input('Введите второе число и нажмите Enter '))
+third=int(input('Введите третье число и нажмите Enter '))
+if first==second==third:
+    print(3)
+elif first==second!=third or first!=second==third or first==third!=second:
+    print(2)
+else:
+    print(0)
